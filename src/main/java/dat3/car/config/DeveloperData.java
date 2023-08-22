9d19b61ba19261c349884c1c87d309b14645f075
Selmerr <115707577+Selmerr@users.noreply.github.com>
2023-08-22 20:32:24 +0200

    First comit

---
+++ src/main/java/dat3/car/config/DeveloperData.java
@@ -0,0 +1,14 @@
+package dat3.car.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DeveloperData implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        System.out.println("fisk");
+    }
+}
